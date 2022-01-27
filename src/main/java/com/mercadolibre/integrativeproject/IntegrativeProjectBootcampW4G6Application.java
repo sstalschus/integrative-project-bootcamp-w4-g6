c6b6b4757b00d0a6e51e352cab512881682b6fc0
Samuel <samuel.stalschus@mercadolivre.com>
2022-01-27 12:04:42 -0300

    Feat: Add - initial commit

---
+++ src/main/java/com/mercadolibre/integrativeproject/IntegrativeProjectBootcampW4G6Application.java
@@ -0,0 +1,12 @@
+package com.mercadolibre.integrativeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrativeProjectBootcampW4G6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegrativeProjectBootcampW4G6Application.class, args);
+    }
+}
---
+++ src/test/java/com/mercadolibre/integrativeproject/IntegrativeProjectBootcampW4G6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.integrativeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrativeProjectBootcampW4G6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
