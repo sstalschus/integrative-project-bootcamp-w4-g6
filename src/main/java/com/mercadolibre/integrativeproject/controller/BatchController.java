96ab7323167890982950d58ecf9b5a2e8ae009f6
arthuramorimmeli <96141013+arthuramorimmeli@users.noreply.github.com>
2022-01-27 15:55:57 -0300

    Add - BatchController and ProductController

---
+++ src/main/java/com/mercadolibre/integrativeproject/controller/BatchController.java
@@ -0,0 +1,26 @@
+package com.mercadolibre.integrativeproject.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BatchController {
+
+    @PostMapping
+    public ResponseEntity<?> create(){
+        return ResponseEntity.ok(null);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getById(){
+        return ResponseEntity.ok(null);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAll(){
+        return ResponseEntity.ok(null);
+    }
+
+}
---
+++ src/main/java/com/mercadolibre/integrativeproject/controller/ProductController.java
@@ -0,0 +1,20 @@
+package com.mercadolibre.integrativeproject.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    public ResponseEntity<?> create(){
+        return ResponseEntity.ok(null);
+    }
+
+    public ResponseEntity<?> getById(){
+        return ResponseEntity.ok(null);
+    }
+
+    public ResponseEntity<?> getAll(){
+        return ResponseEntity.ok(null);
+    }
+}
