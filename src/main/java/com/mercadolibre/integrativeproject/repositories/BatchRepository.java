4c721e01b3aa9d916a7467715e9213d819447719
arthuramorimmeli <96141013+arthuramorimmeli@users.noreply.github.com>
2022-01-28 11:46:06 -0300

    Fix endpoint get all Batches

--- src/main/java/com/mercadolibre/integrativeproject/repositories/BatchRepository.java
+++ src/main/java/com/mercadolibre/integrativeproject/repositories/BatchRepository.java
@@ -1,9 +1,9 @@
 package com.mercadolibre.integrativeproject.repositories;
 
-import com.mercadolibre.integrativeproject.dtos.BatchDTO;
+import com.mercadolibre.integrativeproject.entities.Batch;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BatchRepository extends JpaRepository<BatchDTO, Long> {
+public interface BatchRepository extends JpaRepository<Batch, Long> {
 }
