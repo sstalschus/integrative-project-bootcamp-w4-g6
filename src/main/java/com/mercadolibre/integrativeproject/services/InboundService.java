aafb7862eb1755046d5842d9e84587281ccfb081
lomendes <lorraine.mendes@mercadolivre.com>
2022-01-28 15:17:05 -0300

    Implementacao inbound service

--- src/main/java/com/mercadolibre/integrativeproject/services/InboundService.java
+++ src/main/java/com/mercadolibre/integrativeproject/services/InboundService.java
@@ -1,4 +1,11 @@
 package com.mercadolibre.integrativeproject.services;
 
+import com.mercadolibre.integrativeproject.entities.InboundOrder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class InboundService {
+    public InboundOrder create(InboundOrder inboundOrder){
+        return null;
+    }
 }
