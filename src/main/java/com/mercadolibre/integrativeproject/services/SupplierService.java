41c291a7b9f9e39e0aa44289798213d779c82403
Jefferson Froes <jefferson.froes@mercadolivre.com>
2022-01-31 18:55:13 -0300

    Fix: Add - method for update fixed.
    Doc: Add - created documentation.

--- src/main/java/com/mercadolibre/integrativeproject/services/SupplierService.java
+++ src/main/java/com/mercadolibre/integrativeproject/services/SupplierService.java
@@ -1,20 +1,37 @@
 package com.mercadolibre.integrativeproject.services;
 
 import com.mercadolibre.integrativeproject.entities.Supplier;
+import com.mercadolibre.integrativeproject.exceptions.NotFoundException;
 import com.mercadolibre.integrativeproject.repositories.SupplierRepository;
-import com.mercadolibre.integrativeproject.services.interfaces.ICrudServiceInterface;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.mercadolibre.integrativeproject.services.interfaces.ISupplierService;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/** Service para a entidade de registro de Supplier.
+ *
+ * @author Jefferson Froes
+ *
+ * */
 @Service
-public class SupplierService implements ICrudServiceInterface<Supplier, Long> {
+public class SupplierService implements ISupplierService<Supplier, Long> {
 
-    @Autowired
-     private SupplierRepository supplierRepository;
+    SupplierRepository supplierRepository;
 
+    public SupplierService(SupplierRepository supplierRepository){
+        this.supplierRepository = supplierRepository;
+    }
+
+    /** Método usado para criar um novo registro de Supplier.
+     *
+     * @author Jefferson Froes
+     *
+     * @param  supplier - Registro de supplier.
+     *
+     * @return Registro de supplier criado.
+     *
+     * */
     @Override
     public Supplier create(Supplier supplier) {
         try{
@@ -25,16 +42,29 @@ public class SupplierService implements ICrudServiceInterface<Supplier, Long> {
         }
     }
 
+    /** Método usado para pegar um registro de supplier pelo ID.
+     *
+     * @author Jefferson Froes
+     *
+     * @param supplierId - Id do registro de supplier.
+     *
+     * @return Registro de suppler criado.
+     *
+     * @throws NotFoundException - por id não encontrado.
+     *
+     * */
     @Override
     public Supplier getById(Long supplierId) {
-        try {
-            return supplierRepository.findById(supplierId).orElse(null);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+        return supplierRepository.findById(supplierId).orElseThrow(() -> new NotFoundException("Id not Found"));
     }
 
+    /** Método usado para pegar todos os registros de supplier.
+     *
+     * @author Jefferson Froes
+     *
+     * @return Lista com os registros de supplier.
+     *
+     * */
     @Override
     public List<Supplier> getAll() {
         try {
@@ -45,15 +75,28 @@ public class SupplierService implements ICrudServiceInterface<Supplier, Long> {
         }
     }
 
+    /** Método usado para atualizar o registro supplier.
+     *
+     * @author Jefferson Froes
+     *
+     * @param supplier - objeto que recebe os dados.
+     *
+     * @param supplierId - id do objeto a ser atualizado
+     *
+     * */
     @Override
-    public Supplier update(Supplier supplier) {
-        try {
-            return supplierRepository.save(supplier);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+    public void update(Supplier supplier, Long supplierId) {
+        Supplier supplierSaved = getById(supplierId);
+        supplierRepository.setSupplierInfoById(supplier.getName(), supplier.getCnpj(), supplierSaved.getId());
     }
+
+    /** Método usado para deletar o registro supplier.
+     *
+     * @author Jefferson Froes.
+     *
+     * @param supplierId - id do objeto a ser deletado.
+     *
+     * */
     @Override
     public void delete(Long supplierId) {
         try {
