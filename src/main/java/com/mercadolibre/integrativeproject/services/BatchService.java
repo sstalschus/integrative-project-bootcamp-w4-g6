7ffe0c033e1bb83b56c591c229bf04e3e7a20bda
arthuramorimmeli <96141013+arthuramorimmeli@users.noreply.github.com>
2022-02-01 17:21:40 -0300

    Add - jUnit Services BatchService and ProductService

--- src/main/java/com/mercadolibre/integrativeproject/services/BatchService.java
+++ src/main/java/com/mercadolibre/integrativeproject/services/BatchService.java
@@ -2,75 +2,51 @@ package com.mercadolibre.integrativeproject.services;
 
 import com.mercadolibre.integrativeproject.entities.Batch;
 import com.mercadolibre.integrativeproject.repositories.BatchRepository;
+import com.mercadolibre.integrativeproject.services.interfaces.BathServiceInterface;
 import com.mercadolibre.integrativeproject.services.interfaces.ICrudServiceInterface;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class BatchService implements ICrudServiceInterface<Batch, Long> {
+public class BatchService implements ICrudServiceInterface<Batch, Long>, BathServiceInterface {
 
-    @Autowired
     private BatchRepository batchRepository;
+
+    public BatchService(BatchRepository batchRepository) {
+        this.batchRepository = batchRepository;
+    }
 
     @Override
     public Batch create(Batch batch) {
-        try {
-            return batchRepository.save(batch);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+        return batchRepository.save(batch);
     }
 
     public List<Batch> create(List<Batch> batches){
-        List<Batch>  batchesSaveds = new ArrayList<>();
-        batches.forEach(batch -> batchesSaveds.add(create(batch)));
-
-        return batchesSaveds;
+        return batchRepository.saveAll(batches);
     }
 
     @Override
     public Batch getById(Long batchId) {
-        try {
-            return batchRepository.findById(batchId).orElse(null);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+        return batchRepository.findById(batchId).orElse(null);
     }
 
     @Override
     public List<Batch> getAll() {
-        try {
-            return batchRepository.findAll();
-        } catch (Exception e)  {
-            e.printStackTrace();
-            return new ArrayList<>();
-        }
+        return batchRepository.findAll();
     }
 
     @Override
     public Batch update(Batch batch) {
-        try {
-            return batchRepository.save(batch);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+        return batchRepository.save(batch);
     }
 
     @Override
     public void delete(Long batchId) {
-        try {
-            batchRepository.deleteById(batchId);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        batchRepository.deleteById(batchId);
     }
 
+    @Override
     public Double calcVolumn(List<Batch> batches) {
         return batches.stream().mapToDouble(batchOnSector ->
                 batchOnSector.getQuantity() * batchOnSector.getProduct().getVolumn()).sum();
