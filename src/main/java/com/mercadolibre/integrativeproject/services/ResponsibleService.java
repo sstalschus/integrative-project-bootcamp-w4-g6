5c71c9c7cbf1f7340c33098f08559a0b5edb2eef
Jefferson Froes <jefferson.froes@mercadolivre.com>
2022-02-02 10:09:03 -0300

    Fix: Add - Remove all try-catch Supplier/Responsible

--- src/main/java/com/mercadolibre/integrativeproject/services/ResponsibleService.java
+++ src/main/java/com/mercadolibre/integrativeproject/services/ResponsibleService.java
@@ -7,8 +7,6 @@ import com.mercadolibre.integrativeproject.repositories.ResponsibleRepository;
 import com.mercadolibre.integrativeproject.services.interfaces.IResponsibleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
 
 /** Service para a entidade de registro de Responsible.
@@ -24,24 +22,22 @@ public class ResponsibleService implements IResponsibleService<Responsible, Long
 
     ResponsibleRepository responsibleRepository;
 
-    public ResponsibleService(ResponsibleRepository responsibleRepository){
+    public ResponsibleService(ResponsibleRepository responsibleRepository) {
         this.responsibleRepository = responsibleRepository;
     }
 
-    /** Método usado para criar um novo registro de Responsible.
-     *
-     * @author Jefferson Froes
+    /**
+     * Método usado para criar um novo registro de Responsible.
      *
      * @param responsible - registro de responsible.
-     *
      * @return Registro de responsible criado.
-     *
-     * */
+     * @author Jefferson Froes
+     */
     @Override
     public Responsible create(Responsible responsible, Long sectorId) {
-            Sector sector = sectorService.getById(sectorId);
-            responsible.setSector(sector);
-            return responsibleRepository.save(responsible);
+        Sector sector = sectorService.getById(sectorId);
+        responsible.setSector(sector);
+        return responsibleRepository.save(responsible);
     }
 
     @Override
@@ -50,52 +46,38 @@ public class ResponsibleService implements IResponsibleService<Responsible, Long
         responsibleRepository.setResponsibleChangeSector(responsibleId, sectorId);
     }
 
-    /** Método usado para buscar um registro de responsible pelo id.
-     *
-     * @author Jefferson Froes
+    /**
+     * Método usado para buscar um registro de responsible pelo id.
      *
      * @param responsibleId - id para busca.
-     *
      * @return Registro de responsible por id.
-     *
-     * */
+     * @author Jefferson Froes
+     */
     @Override
     public Responsible getById(Long responsibleId) {
-            return responsibleRepository.findById(responsibleId).orElseThrow(() -> new NotFoundException("Id not Found."));
+        return responsibleRepository.findById(responsibleId).orElseThrow(() -> new NotFoundException("Id not Found."));
     }
 
-    /** Método usado para pegar todos os registros de responsible.
-     *
-     * @author Jefferson Froes
+    /**
+     * Método usado para pegar todos os registros de responsible.
      *
      * @return Lista com os registros de responsible.
-     *
-     * */
+     * @author Jefferson Froes
+     */
     @Override
     public List<Responsible> getAll() {
-        try {
-            return responsibleRepository.findAll();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new ArrayList<>();
-        }
+        return responsibleRepository.findAll();
     }
 
 
-
-    /** Método usado para deletar o registro responsible.
-     *
-     * @author Jefferson Froes.
+    /**
+     * Método usado para deletar o registro responsible.
      *
      * @param responsibleId - id do objeto a ser deletado
-     *
-     * */
+     * @author Jefferson Froes.
+     */
     @Override
     public void delete(Long responsibleId) {
-        try{
-            responsibleRepository.deleteById(responsibleId);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        responsibleRepository.deleteById(responsibleId);
     }
 }
