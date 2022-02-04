23cd3f21a5a3708de824267ffb2ea9ba72330d22
Samuel Stalschus <samuel.stalschus@mercadolivre.com>
2022-02-04 15:11:01 -0300

    Feat: Authenticate

---
+++ src/main/java/com/mercadolibre/integrativeproject/config/security/WebSecurityConfig.java
@@ -0,0 +1,10 @@
+package com.mercadolibre.integrativeproject.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+}
