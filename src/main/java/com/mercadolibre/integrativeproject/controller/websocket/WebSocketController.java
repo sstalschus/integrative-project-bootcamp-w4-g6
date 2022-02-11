ed678abdff2a4255b7a484df7dcde0d6d61748fb
arthuramorimmeli <96141013+arthuramorimmeli@users.noreply.github.com>
2022-02-11 09:41:39 -0300

    Add - commentarios e dividido metodo

--- src/main/java/com/mercadolibre/integrativeproject/controller/websocket/WebSocketController.java
+++ src/main/java/com/mercadolibre/integrativeproject/controller/websocket/WebSocketController.java
@@ -1,26 +1,20 @@
 package com.mercadolibre.integrativeproject.controller.websocket;
 
-import com.google.gson.Gson;
 import com.mercadolibre.integrativeproject.entities.Batch;
 import com.mercadolibre.integrativeproject.entities.MessageModel;
 import com.mercadolibre.integrativeproject.entities.Sector;
 import com.mercadolibre.integrativeproject.services.SectorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.converter.SimpleMessageConverter;
-import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
-import java.security.Principal;
 import java.util.Comparator;
-import java.util.Map;
-
+/** Controller WebSocket
+ *
+ * @author Arthur Amorim
+ *
+ * */
 @Controller
 public class WebSocketController {
 
@@ -30,27 +24,72 @@ public class WebSocketController {
     @Autowired
     private SectorService sectorService;
 
+    /** Método usado para alterar a temperatura de um Setor
+     * e publicar esta alteracao no topico de notificacao.
+     *
+     * @param message - Mensagem recebida por um cliente via WebSocket
+     *
+     * @author Arthur Amorim
+     *
+     *
+     * */
     @MessageMapping("/warn")
     public void sendMessage(MessageModel message) {
-        message = getWarnMesage(message);
+        message = updateTemperature(message);
         simpMessagingTemplate.convertAndSend("/topic/notify", message);
     }
 
-    private MessageModel getWarnMesage(MessageModel warnMesage) {
+    /** Método usado para alterar a temperatura de um Setor
+     * e publicar esta alteracao no topico de notificacao.
+     *
+     * @param warnMesage - Mensagem recebida por um cliente via WebSocket
+     *
+     * @author Arthur Amorim
+     *
+     *
+     * */
+    private MessageModel updateTemperature(MessageModel warnMesage) {
         Sector sector = sectorService.getById(warnMesage.getSectorId());
         if (warnMesage.getTemperature() != null) {
-            sector.setTemperature(warnMesage.getTemperature());
-            sectorService.updateTemperature(sector.getId(), sector.getTemperature());
+            updateTemperatureOnSector(warnMesage, sector);
             Batch batch = sector.getLots().stream().max(Comparator.comparingDouble(Batch::getMinimumTemperature)).orElse(null);
-            if (batch != null) {
-                warnMesage.setMinimumTemperatureNecessarie(batch.getMinimumTemperature());
-                if (batch.getMinimumTemperature() < warnMesage.getTemperature()) {
-                    warnMesage.setStatus("WARN");
-                }else {
-                    warnMesage.setStatus("OK");
-                }
-            }
+            setStatusMessage(warnMesage, batch);
         }
         return warnMesage;
+    }
+
+    /** Método usado para alterar a temperatura de um Setor
+     * e publicar esta alteracao no topico de notificacao.
+     *
+     * @param warnMesage - Mensagem com informacoes sobre a atualizacao
+     *
+     * @author Arthur Amorim
+     *
+     *
+     * */
+    private void updateTemperatureOnSector(MessageModel warnMesage, Sector sector) {
+        sector.setTemperature(warnMesage.getTemperature());
+        sectorService.updateTemperature(sector.getId(), sector.getTemperature());
+    }
+
+    /** Método usado para verificar se a nova temperatura ira manter os lotes
+     * preservados
+     *
+     * @param warnMesage - Mensagem com informacoes sobre a atualizacao.
+     * @param batch - Loque com a menor temperatura do respectivo setor
+     *
+     * @author Arthur Amorim
+     *
+     *
+     * */
+    private void setStatusMessage(MessageModel warnMesage, Batch batch) {
+        if (batch != null) {
+            warnMesage.setMinimumTemperatureNecessarie(batch.getMinimumTemperature());
+            if (batch.getMinimumTemperature() < warnMesage.getTemperature()) {
+                warnMesage.setStatus("WARN");
+            }else {
+                warnMesage.setStatus("OK");
+            }
+        }
     }
 }
